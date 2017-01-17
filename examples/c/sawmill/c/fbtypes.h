f8c73654ba8e424c0e7e917796c9ec750f2ab96e
kiwih <hammondpearce@outlook.com>
2017-01-17 15:56:39 +1300

    add switches to fbtypes

--- examples/c/sawmill/c/fbtypes.h
+++ examples/c/sawmill/c/fbtypes.h
@@ -13,6 +13,7 @@
 
 #define LED ( *( ( volatile _IODEV unsigned * )	0xF0090000 ) )
 #define HEX ( *( ( volatile _IODEV unsigned * )	0xF0070000 ) )
+#define SWITCHES ( *( ( volatile _IODEV unsigned * )	0xF0060000 ) )
 
 /*********************************************************************
  This file contains the mapping between IEC 61131 and C data types.
