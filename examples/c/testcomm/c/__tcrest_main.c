b755a4b90401d475974bd26d1dcab56474d9c8a8
kiwih <hammondpearce@outlook.com>
2017-01-25 14:00:33 +1300

    Make testcomm work with pointers

--- examples/c/testcomm/c/__tcrest_main.c
+++ examples/c/testcomm/c/__tcrest_main.c
@@ -1,9 +1,9 @@
 //This is the main file for the iec61499 network with _TCREST as the top level block
 
-#include "_TCREST.h"
+#include "_Core0.h"
+#include "_Core1.h"
 
 //put a copy of the top level block into global memory
-_TCREST_t my_TCREST;
 
 const int NOC_MASTER = 0;
 
@@ -13,8 +13,8 @@ volatile _UNCACHED int c1init = 0;
 void t0(void* param);
 void t1(void* param);
 
-void task0(void* param);
-void task1(void* param);
+void task0(_Core0_t * c0);
+void task1(_Core1_t * c1);
 
 int main() {
 	printf("testcomm startup.\n");
@@ -26,13 +26,13 @@ int main() {
 	t0(NULL);
 }
 
-void __attribute__ ((noinline)) timed_task() {
-	_Core0_syncEvents(&my_TCREST.rx_core);
-	_Core0_syncData(&my_TCREST.rx_core);
-	_Core0_run(&my_TCREST.rx_core);
+void __attribute__ ((noinline)) timed_task(_Core0_t * c0) {
+	_Core0_syncEvents(c0);
+	_Core0_syncData(c0);
+	_Core0_run(c0);
 }
 
-void task0(void* param) {
+void task0(_Core0_t * c0) {
 	//task0 runs core0
 	unsigned int tickCount = 0;
 
@@ -42,7 +42,7 @@ void task0(void* param) {
 	do {
 		start_time = get_cpu_cycles();
 
-		timed_task();
+		timed_task(c0);
 
 		end_time = get_cpu_cycles();
 		//printf("%4d\t\t%lld\n", tickCount, end_time-start_time-3);
@@ -52,37 +52,41 @@ void task0(void* param) {
 }
 
 
-void task1(void* param) {
+void task1(_Core1_t * c1) {
 	//task1 runs core1
 
 	unsigned int tickCount = 0;
 	do {
-		_Core1_syncEvents(&my_TCREST.tx_core);
-		_Core1_syncData(&my_TCREST.tx_core);
-		_Core1_run(&my_TCREST.tx_core);
+		_Core1_syncEvents(c1);
+		_Core1_syncData(c1);
+		_Core1_run(c1);
 	} while(1);
 }
 
 void t1(void* param) {
+	_Core1_t c1;
 
-	_Core1_init(&my_TCREST.tx_core);
+	_Core1_preinit(&c1);
+	_Core1_init(&c1);
 	mp_init_ports();
 
 	c1init = 1;
 	while(c0init != 1 && c1init != 1);
 
-	task1(NULL);
+	task1(&c1);
 
 }
 
 
 void t0(void* param) {
+	_Core0_t c0;
 
-	_Core0_init(&my_TCREST.rx_core);
+	_Core0_preinit(&c0);
+	_Core0_init(&c0);
 	mp_init_ports();
 
 	c0init = 1;
 	while(c0init != 1 && c1init != 1);
 
-	task0(NULL);
+	task0(&c0);
 }
