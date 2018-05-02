8dd86d768a5ccd5a112a703c0f044d778e5f969a
kiwih <hammondpearce@outlook.com>
2018-05-02 16:16:00 +1200

    adding thoughts about compilation

--- iec61499/pfbToBfb.go
+++ iec61499/pfbToBfb.go
@@ -2,7 +2,61 @@ package iec61499
 
 import "errors"
 
-//TranslatePFBtoBFB will take an Policyr Function Block and compile it to its equivalent
+/* an example of a conversion
+
+policyFB AEIPolicy;
+interface of AEIPolicy {
+	in event AS, VS; //in here means that they're going from PLANT to CONTROLLER
+	out event AP, VP;//out here means that they're going from CONTROLLER to PLANT
+}
+architecture of AEIPolicy {
+	internals {
+		ulint AEI_ns := 900000000;
+		dtimer tAEI; //DTIMER increases in DISCRETE TIME continuously
+	}
+
+	//P3: AS or AP must be true within AEI after a ventricular event VS or VP.
+
+	states {
+		s1 {
+			//-> <destination> [on guard] [: output expression][, output expression...] ;
+			-> s2 on (VS or VP): tAEI := 0;
+		}
+
+		s2 {
+			-> s1 on (AS or AP);
+			-> violation on (tAEI > AEI_ns);
+		}
+	}
+}
+
+SHOULD BECOME
+
+basicFB AEIPolicyEnforcer;
+interface of AEIPolicyEnforcer {
+	in event AS_poci_in, VS_poci_in; //PLANT OUT CONTROLLER IN, IN/OUT w.r.t. ENFORCER
+	out event AS_poci_out, VS_poci_out;
+
+	in event AP_pico_in, VP_pico_in; //PLANT IN CONTROLLER OUT, IN/OUT w.r.t. ENFORCER
+	out event AP_pico_out, VP_pico_out;
+}
+architecture of AEIPolicyEnforcer {
+	internals {
+		ulint AEI_ns := 900000000;
+		dtimer tAEI; //DTIMER increases in DISCRETE TIME continuously
+	}
+
+	states {
+		s1 {
+			-> s1 on (VS or VP);
+			run
+		}
+	}
+}
+
+*/
+
+//TranslatePFBtoBFB will take a Policy Function Block and compile it to its equivalent
 // Basic Function Block
 //It operates according to the algorithm specified in [TODO: Paper link]
 func (f *FB) TranslatePFBtoBFB() error {
