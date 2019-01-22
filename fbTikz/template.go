f9146381644ce757643c6bc81b036958a32cbb8b
kiwih <hammondpearce@outlook.com>
2019-01-22 22:07:56 +1300

    fbTikz link line colours

--- fbTikz/template.go
+++ fbTikz/template.go
@@ -6,18 +6,24 @@ import (
 )
 
 const tikzTemplateStr = `\documentclass{standalone}
+\usepackage[rgb]{xcolor}
 \usepackage{tikz}
 
 \begin{document}
 \begin{tikzpicture}[x=5mm,y=-5mm]
+\definecolor{eventWire}{HTML}{6C8EBF}
+\definecolor{dataWire}{HTML}{B85450}
+
 {{$width := 7}}
 \draw (0,0) -- ({{$width}},0); %top bar
 
 %top sides and event I/O
 {{$varLen := .GetEventsSize -}}
 {{range $i, $names := .GetMatchedEvents}}
-\draw (0,{{$i}}) -- (0,{{add $i 1}}) {{if $names.Input}}node [anchor=west] { {{textsafe $names.Input}} } -- (-1,{{add $i 1}}) {{end}}; %vert line and label and link line if necessary
-\draw (({{$width}},{{$i}}) -- (({{$width}},{{add $i 1}}) {{if $names.Output}}node [anchor=east] { {{textsafe $names.Output}} } -- ({{add $width 1}},{{add $i 1}}) {{end}}; 
+\draw (0,{{$i}}) -- (0,{{add $i 1}}) {{if $names.Input}}node [anchor=west] { {{textsafe $names.Input}} } {{end}}; %vert line and label 
+{{if $names.Input}}\draw [eventWire] (0,{{add $i 1}}) -- (-1,{{add $i 1}}); %link line {{end}}
+\draw (({{$width}},{{$i}}) -- (({{$width}},{{add $i 1}}) {{if $names.Output}}node [anchor=east] { {{textsafe $names.Output}} } {{end}}; %vert line and label 
+{{if $names.Output}}\draw [eventWire] (({{$width}},{{add $i 1}}) -- ({{add $width 1}},{{add $i 1}}); %link line {{end}}
 {{end}}
 
 %left indent
@@ -35,8 +41,10 @@ const tikzTemplateStr = `\documentclass{standalone}
 %bottom sides and bottom I/O
 {{$baseVars := add $varLen 2}}{{$eventLen := .GetVarsSize -}}
 {{range $i, $names := .GetMatchedVars}}
-\draw (0,{{add $baseVars $i}}) -- (0,{{add $i (add $baseVars 1)}}) {{if $names.Input}}node [anchor=west,yshift=-0.5] { {{textsafe $names.Input}} } -- (-1,{{add $i (add $baseVars 1)}}) {{end}}; 
-\draw ({{$width}},{{add $baseVars $i}}) -- ({{$width}},{{add $i (add $baseVars 1)}}) {{if $names.Output}}node [anchor=east,yshift=-0.5] { {{textsafe $names.Output}} } -- ({{add $width 1}},{{add $i (add $baseVars 1)}}) {{end}}; 
+\draw (0,{{add $baseVars $i}}) -- (0,{{add $i (add $baseVars 1)}}) {{if $names.Input}}node [anchor=west,yshift=-0.5] { {{textsafe $names.Input}} } {{end}}; %vert line and label 
+{{if $names.Input}}\draw [dataWire] (0,{{add $i (add $baseVars 1)}}) -- (-1,{{add $i (add $baseVars 1)}}); %link line {{end}}
+\draw ({{$width}},{{add $baseVars $i}}) -- ({{$width}},{{add $i (add $baseVars 1)}}) {{if $names.Output}}node [anchor=east,yshift=-0.5] { {{textsafe $names.Output}} } {{end}}; 
+{{if $names.Output}}\draw [dataWire] ({{$width}},{{add $i (add $baseVars 1)}}) -- ({{add $width 1}},{{add $i (add $baseVars 1)}}); %link line {{end}}
 {{end}}
 
 %bottom container
