dbb25a0428744b86b97a01d8dafb21c36399530f
kiwih <hammondpearce@outlook.com>
2019-01-24 10:18:32 +1300

    fbTikz complete refactor to greatly simplify template, still need to redo ports

--- fbTikz/fbTikz.go
+++ fbTikz/fbTikz.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/xml"
 	"errors"
+	"fmt"
 
 	"github.com/PRETgroup/goFB/iec61499"
 )
@@ -39,8 +40,9 @@ func (f *FBTikz) ConvertAll() ([]OutputFile, error) {
 	for i := 0; i < len(f.Blocks); i++ {
 
 		output := &bytes.Buffer{}
-
-		if err := tikzTemplate.ExecuteTemplate(output, "", FBTikzHelper(f.Blocks[i])); err != nil {
+		origin := FBTikzPoint{0.0, 0.0}
+		fbh := NewFBTikzHelper(f.Blocks[i], origin)
+		if err := tikzTemplate.ExecuteTemplate(output, "", fbh); err != nil {
 			return nil, errors.New("Couldn't format template (fb) of" + f.Blocks[i].Name + ": " + err.Error())
 		}
 
@@ -51,7 +53,162 @@ func (f *FBTikz) ConvertAll() ([]OutputFile, error) {
 }
 
 //FBTikzHelper overleads the iec61499.FB type with extra helper functions
-type FBTikzHelper iec61499.FB
+type FBTikzHelper struct {
+	iec61499.FB
+	Points FBTikzPoints
+}
+
+//FBTikzPoint represents a point in Tikz-space (i.e. X,Y)
+type FBTikzPoint struct {
+	X float64
+	Y float64
+}
+
+//AddX adds x to a given point and returns it as a new point
+func (p FBTikzPoint) AddX(x float64) FBTikzPoint {
+	return FBTikzPoint{X: p.X + x, Y: p.Y}
+}
+
+//AddY adds y to a given point and returns it as a new point
+func (p FBTikzPoint) AddY(y float64) FBTikzPoint {
+	return FBTikzPoint{X: p.X, Y: p.Y + y}
+}
+
+//Add adds x,y to a given point and returns it as a new point
+func (p FBTikzPoint) Add(x, y float64) FBTikzPoint {
+	return FBTikzPoint{X: p.X + x, Y: p.Y + y}
+}
+
+func (p FBTikzPoint) String() string {
+	return fmt.Sprintf("(%.2f,%.2f)", p.X, p.Y)
+}
+
+//Render constants
+const (
+	NeckInset       float64 = 1
+	NeckHeight      float64 = 1
+	TextSpacing     float64 = 1
+	TextOffset      float64 = 0.5
+	MinIOLineLength float64 = 1
+	MinBlockWidth   float64 = 7
+)
+
+/*
+                                  width
+                |--------------------|
+
+---		origin: +--------------------+   ---
+ |				|                    |    |
+ |				| a               c  |   --- textoffset, to calculate (origin + offsety) or (origin + textoffset + textspacing * portoffset)
+ |				|                    |    |
+ |				| b               d  |   --- textspacing
+ |		 		|                    |
+---eventheight:	+----+          +----+
+ | 			 		 |          |
+---	neckheight:	+----+          +----+
+ |				|                    |
+ |				|                 e  |   --- to calculate, (origin + offsety) or (origin + eventheight + neckheight + textoffset + textspacing * portoffset)
+ |				|                    |
+ |				|                 f  |
+ |				|                    |
+---	dataheight:	+--------------------+
+
+*/
+
+//FBTikzPoints is used to store information about where everything is for a given FB
+type FBTikzPoints struct {
+	Origin          FBTikzPoint //x = xlocation, y = ylocation
+	Width           float64     // == width of the FB
+	EventsHeight    float64     // == height of the events area
+	NeckHeight      float64     // == height of the fb "neck"
+	DataHeight      float64     // == height of the data area
+	TextSpacing     float64     // == spacing between i/o labels
+	TextOffset      float64     // == offset from top of area to first text label
+	MinIOLineLength float64     // == minimum length of the i/o lines
+	EventsInfo      map[string]FBTikzIOInfo
+	Border          FBTikzBorderBox
+}
+
+//FBTikzIOInfo is used for port info, location, data-variable association
+type FBTikzIOInfo struct {
+	OffsetY    float64 //the difference between the block origin and the ypos for this position
+	LinkX      float64 //if 0, not linked, if !0, this is the position to render the link in
+	LinkEventY float64 //if 0, this is the event, and no action required, if !0, this is the height to draw the vertical association link to
+}
+
+//NewFBTikzHelper will convert an FB to a FBTikzHelper and calculate all
+//necessary render information
+func NewFBTikzHelper(fb iec61499.FB, origin FBTikzPoint) FBTikzHelper {
+	help := FBTikzHelper{
+		FB: fb,
+		Points: FBTikzPoints{
+			Origin:      origin,
+			TextSpacing: TextSpacing,
+			TextOffset:  TextOffset,
+			NeckHeight:  NeckHeight,
+			EventsInfo:  make(map[string]FBTikzIOInfo),
+		},
+	}
+
+	//calculate width
+	help.Points.Width = 7
+
+	//calculate events height
+	eventLength := help.GetEventsSize() + 1
+	help.Points.EventsHeight = float64(eventLength) * (help.Points.TextSpacing)
+
+	//calculate data height
+	dataLength := help.GetVarsSize() + 1
+	help.Points.DataHeight = float64(dataLength) * (help.Points.TextSpacing)
+
+	for i, port := range help.FB.InterfaceList.EventInputs {
+		info := FBTikzIOInfo{}
+		info.OffsetY = help.Points.TextOffset + float64(i)*help.Points.TextSpacing
+		help.Points.EventsInfo[port.Name] = info
+	}
+
+	for i, port := range help.FB.InterfaceList.EventOutputs {
+		info := FBTikzIOInfo{}
+		info.OffsetY = help.Points.TextOffset + float64(i)*help.Points.TextSpacing
+		help.Points.EventsInfo[port.Name] = info
+	}
+
+	//calculate border
+	help.Points.Border = FBTikzBorderBox{}
+
+	help.Points.Border.EventsTopLeft = help.Points.Origin
+	help.Points.Border.EventsTopRight = help.Points.Origin.AddX(help.Points.Width)
+	help.Points.Border.EventsBottomLeft = help.Points.Border.EventsTopLeft.AddY(help.Points.EventsHeight)
+	help.Points.Border.EventsBottomRight = help.Points.Border.EventsTopRight.AddY(help.Points.EventsHeight)
+
+	help.Points.Border.NeckTopLeft = help.Points.Border.EventsBottomLeft.AddX(NeckInset)
+	help.Points.Border.NeckTopRight = help.Points.Border.EventsBottomRight.AddX(-NeckInset)
+	help.Points.Border.NeckBottomLeft = help.Points.Border.NeckTopLeft.AddY(help.Points.NeckHeight)
+	help.Points.Border.NeckBottomRight = help.Points.Border.NeckTopRight.AddY(help.Points.NeckHeight)
+
+	help.Points.Border.DataTopLeft = help.Points.Border.EventsBottomLeft.AddY(help.Points.NeckHeight)
+	help.Points.Border.DataTopRight = help.Points.Border.EventsBottomRight.AddY(help.Points.NeckHeight)
+	help.Points.Border.DataBottomLeft = help.Points.Border.DataTopLeft.AddY(help.Points.DataHeight)
+	help.Points.Border.DataBottomRight = help.Points.Border.DataTopRight.AddY(help.Points.DataHeight)
+
+	return help
+}
+
+//FBTikzBorderBox contains the list of points that make up the border of the structure
+type FBTikzBorderBox struct {
+	EventsTopLeft     FBTikzPoint
+	EventsTopRight    FBTikzPoint
+	EventsBottomLeft  FBTikzPoint
+	EventsBottomRight FBTikzPoint
+	NeckTopLeft       FBTikzPoint
+	NeckTopRight      FBTikzPoint
+	NeckBottomLeft    FBTikzPoint
+	NeckBottomRight   FBTikzPoint
+	DataTopLeft       FBTikzPoint
+	DataTopRight      FBTikzPoint
+	DataBottomLeft    FBTikzPoint
+	DataBottomRight   FBTikzPoint
+}
 
 //GetVarsSize returns the height of the vars area that needs to be drawen
 func (f FBTikzHelper) GetVarsSize() int {
--- fbTikz/template.go
+++ fbTikz/template.go
@@ -13,69 +13,20 @@ const tikzTemplateStr = `\documentclass{standalone}
 \begin{tikzpicture}[x=5mm,y=-5mm]
 \definecolor{eventWire}{HTML}{6C8EBF}
 \definecolor{dataWire}{HTML}{B85450}
-
-{{$width := 7}}{{$io := .GetTikzIO}}
-\draw (0,0) -- ({{$width}},0); %top bar
-
-%top sides and event I/O
-{{$varLen := .GetEventsSize -}}
-{{range $i, $names := $io.Events}}
-\draw (0,{{$i}}) -- (0,{{add $i 1}}) {{if $names.Input}}node [anchor=west] { {{textsafe $names.Input}} } {{end}}; %vert line and label 
-{{if $names.Input}}
-	\draw [eventWire] (0,{{add $i 1}}) -- (-1,{{add $i 1}}); %link line
-	{{if $names.InputAssoc.PosX}}
-		\draw ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $i 1}}) circle (0.3mm); %association circle
-	{{end}}
-{{end}}
-\draw (({{$width}},{{$i}}) -- (({{$width}},{{add $i 1}}) {{if $names.Output}}node [anchor=east] { {{textsafe $names.Output}} } {{end}}; %vert line and label 
-{{if $names.Output}}
-	\draw [eventWire] (({{$width}},{{add $i 1}}) -- ({{add $width 1}},{{add $i 1}}); %link line 
-	{{if $names.OutputAssoc.PosX}}
-		\draw ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $i 1}}) circle (0.3mm); %association circle
-	{{end}}
-{{end}}
-{{end}}
-
-%left indent
-\draw (0,{{$varLen}}) -- (0,{{add $varLen 1}});
-\draw (0,{{add $varLen 1}}) -- (1,{{add $varLen 1}});
-\draw (1,{{add $varLen 1}}) -- (1,{{add $varLen 2}});
-\draw (1,{{add $varLen 2}}) -- (0,{{add $varLen 2}});
-
-%right indent
-\draw (({{$width}},{{$varLen}}) -- ({{$width}},{{add $varLen 1}});
-\draw (({{$width}},{{add $varLen 1}}) -- ({{sub $width 1}},{{add $varLen 1}});
-\draw (({{sub $width 1}},{{add $varLen 1}}) -- ({{sub $width 1}},{{add $varLen 2}});
-\draw (({{sub $width 1}},{{add $varLen 2}}) -- ({{$width}},{{add $varLen 2}});
-
-%bottom sides and bottom I/O
-{{$baseVars := add $varLen 2}}{{$eventLen := .GetVarsSize -}}
-{{range $i, $names := $io.Data}}
-\draw (0,{{add $baseVars $i}}) -- (0,{{add $i (add $baseVars 1)}}) {{if $names.Input}}node [anchor=west,yshift=-0.5] { {{textsafe $names.Input}} } {{end}}; %vert line and label 
-{{if $names.Input}}
-\draw [dataWire] (0,{{add $i (add $baseVars 1)}}) -- (-1,{{add $i (add $baseVars 1)}}); %link line 
-	{{if $names.InputAssoc.PosX}}
-		\draw ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $i (add $baseVars 1)}}) circle (0.3mm); %association circle
-	{{end}}
-	\draw [dataWire,dashed] ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $i (add $baseVars 1)}}) 
-		-- ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $names.InputAssoc.PosEvent 1}}); %association line
-{{end}}
-\draw ({{$width}},{{add $baseVars $i}}) -- ({{$width}},{{add $i (add $baseVars 1)}}) {{if $names.Output}}node [anchor=east,yshift=-0.5] { {{textsafe $names.Output}} } {{end}}; 
-{{if $names.Output}}
-\draw [dataWire] ({{$width}},{{add $i (add $baseVars 1)}}) -- ({{add $width 1}},{{add $i (add $baseVars 1)}}); %link line 
-	{{if $names.OutputAssoc.PosX}}
-	\draw ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $i (add $baseVars 1)}}) circle (0.3mm); %association circle
-	{{end}}
-	\draw [dataWire,dashed] ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $i (add $baseVars 1)}}) 
-		-- ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $names.InputAssoc.PosEvent 1}}); %association line
-
-{{end}}
-{{end}}
-
-%bottom container
-\draw (0,{{add $baseVars $eventLen}}) -- (0,{{add $baseVars (add $eventLen 1)}}); %left
-\draw ({{$width}},{{add $baseVars $eventLen}}) -- ({{$width}},{{add $baseVars (add $eventLen 1)}}); %right
-\draw (0,{{add $baseVars (add $eventLen 1)}}) -- ({{$width}},{{add $baseVars (add $eventLen 1)}}); %bottom bar
+{{$border := .Points.Border}}
+\draw 	{{$border.EventsTopLeft}} -- 
+		{{$border.EventsTopRight}} --
+		{{$border.EventsBottomRight}} --
+		{{$border.NeckTopRight}} --
+		{{$border.NeckBottomRight}} --
+		{{$border.DataTopRight}} --
+		{{$border.DataBottomRight}} --
+		{{$border.DataBottomLeft}} --
+		{{$border.DataTopLeft}} --
+		{{$border.NeckBottomLeft}} --
+		{{$border.NeckTopLeft}} --
+		{{$border.EventsBottomLeft}} --
+		cycle;
 
 \end{tikzpicture}
 \end{document}
@@ -120,3 +71,70 @@ func textsafe(s string) string {
 }
 
 var tikzTemplate = template.Must(template.New("").Funcs(tikzTemplateFuncMap).Parse(tikzTemplateStr))
+
+/*
+{{$width := 7}}{{$io := .GetTikzIO}}
+\draw (0,0) -- ({{$width}},0); %top bar
+
+%top sides and event I/O
+{{$varLen := .GetEventsSize -}}
+{{range $i, $names := $io.Events}}
+\draw (0,{{$i}}) -- (0,{{add $i 1}}) {{if $names.Input}}node [anchor=west] { {{textsafe $names.Input}} } {{end}}; %vert line and label
+{{if $names.Input}}
+	\draw [eventWire] (0,{{add $i 1}}) -- (-1,{{add $i 1}}); %link line
+	{{if $names.InputAssoc.PosX}}
+		\draw ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $i 1}}) circle (0.3mm); %association circle
+	{{end}}
+{{end}}
+\draw (({{$width}},{{$i}}) -- (({{$width}},{{add $i 1}}) {{if $names.Output}}node [anchor=east] { {{textsafe $names.Output}} } {{end}}; %vert line and label
+{{if $names.Output}}
+	\draw [eventWire] (({{$width}},{{add $i 1}}) -- ({{add $width 1}},{{add $i 1}}); %link line
+	{{if $names.OutputAssoc.PosX}}
+		\draw ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $i 1}}) circle (0.3mm); %association circle
+	{{end}}
+{{end}}
+{{end}}
+
+%left indent
+\draw (0,{{$varLen}}) -- (0,{{add $varLen 1}});
+\draw (0,{{add $varLen 1}}) -- (1,{{add $varLen 1}});
+\draw (1,{{add $varLen 1}}) -- (1,{{add $varLen 2}});
+\draw (1,{{add $varLen 2}}) -- (0,{{add $varLen 2}});
+
+%right indent
+\draw (({{$width}},{{$varLen}}) -- ({{$width}},{{add $varLen 1}});
+\draw (({{$width}},{{add $varLen 1}}) -- ({{sub $width 1}},{{add $varLen 1}});
+\draw (({{sub $width 1}},{{add $varLen 1}}) -- ({{sub $width 1}},{{add $varLen 2}});
+\draw (({{sub $width 1}},{{add $varLen 2}}) -- ({{$width}},{{add $varLen 2}});
+
+%bottom sides and bottom I/O
+{{$baseVars := add $varLen 2}}{{$eventLen := .GetVarsSize -}}
+{{range $i, $names := $io.Data}}
+\draw (0,{{add $baseVars $i}}) -- (0,{{add $i (add $baseVars 1)}}) {{if $names.Input}}node [anchor=west,yshift=-0.5] { {{textsafe $names.Input}} } {{end}}; %vert line and label
+{{if $names.Input}}
+\draw [dataWire] (0,{{add $i (add $baseVars 1)}}) -- (-1,{{add $i (add $baseVars 1)}}); %link line
+	{{if $names.InputAssoc.PosX}}
+		\draw ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $i (add $baseVars 1)}}) circle (0.3mm); %association circle
+	{{end}}
+	\draw [dataWire,dashed] ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $i (add $baseVars 1)}})
+		-- ({{subf -0.2 (mulf 0.2 (intf $names.InputAssoc.PosX))}},{{add $names.InputAssoc.PosEvent 1}}); %association line
+{{end}}
+\draw ({{$width}},{{add $baseVars $i}}) -- ({{$width}},{{add $i (add $baseVars 1)}}) {{if $names.Output}}node [anchor=east,yshift=-0.5] { {{textsafe $names.Output}} } {{end}};
+{{if $names.Output}}
+\draw [dataWire] ({{$width}},{{add $i (add $baseVars 1)}}) -- ({{add $width 1}},{{add $i (add $baseVars 1)}}); %link line
+	{{if $names.OutputAssoc.PosX}}
+	\draw ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $i (add $baseVars 1)}}) circle (0.3mm); %association circle
+	{{end}}
+	\draw [dataWire,dashed] ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $i (add $baseVars 1)}})
+		-- ({{addf (addf (intf $width) 0.2) (mulf 0.2 (intf $names.OutputAssoc.PosX))}},{{add $names.InputAssoc.PosEvent 1}}); %association line
+
+{{end}}
+{{end}}
+
+%bottom container
+\draw (0,{{add $baseVars $eventLen}}) -- (0,{{add $baseVars (add $eventLen 1)}}); %left
+\draw ({{$width}},{{add $baseVars $eventLen}}) -- ({{$width}},{{add $baseVars (add $eventLen 1)}}); %right
+\draw (0,{{add $baseVars (add $eventLen 1)}}) -- ({{$width}},{{add $baseVars (add $eventLen 1)}}); %bottom bar
+
+
+*/
